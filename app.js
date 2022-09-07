e090e4942f371026342b2b2f4f7e1d36859eeccc
Bigu Theodor <btheodor@adobe.com>
2022-09-07 16:50:37 +0300

    aos-defer

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const animItemRight = bodymovin.loadAnimation({
 
 AOS.init({
   offset: 200, // offset (in px) from the original trigger point
-  delay: 200, // values from 0 to 3000, with step 50ms
+  delay: 0, // values from 0 to 3000, with step 50ms
   duration: 500, // values from 0 to 3000, with step 50ms
   easing: "ease-out", // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
