9c9d8eda4973c6188adc4843b6d5629e429fc399
Bigu Theodor <btheodor@adobe.com>
2022-09-07 11:40:34 +0300

    aos-test

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ toggleButton.addEventListener("click", () => {
   navbar.classList.toggle("navbar-bg");
 });
 
-
 // lottie-hero:
 
 const svgContainerHero = document.getElementById("hero-lottie");
@@ -20,8 +19,6 @@ const animItemHero = bodymovin.loadAnimation({
   autoplay: true,
   path: "./lotties/hero-arrow.json",
 });
-
-
 
 // lottie-galerie:
 
@@ -43,5 +40,14 @@ const animItemRight = bodymovin.loadAnimation({
   path: "../lotties/galerie.json",
 });
 
+//AOS:
 
-
+AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
